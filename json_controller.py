998f9bca700399bad959941963266b17e18207c6
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-07-28 09:23:12 -0400

    Update json_controller.py (I fixed names I’m on phone so I hope that broke nothing)

--- json_controller.py
+++ json_controller.py
@@ -1,20 +1,20 @@
 import json
 
-def read_data_json(filename):
-    with open(filename, 'r') as f:
-        data = json.load(f)
+def read_data_json(filename): # am i the only person who copy pastes this
+    with open(filename, 'r') as f: # from project to project making only minor
+        data = json.load(f) # Changes
     return data
 
 def write_data_json(filename, new_entry):
-    username = new_entry["username"]
-    new_score = int(new_entry["score"])
+    username = entry["username"]
+    score = int(entry["score"])
 
     data = read_data_json(filename)
 
     if username not in data:
         data[username] = {"scores": []}
 
-    data[username]["scores"].append(new_score)
+    data[username]["scores"].append(score)
 
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
